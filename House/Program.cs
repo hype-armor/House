f7f741c3166b2023b063cccd611b55a9519a1f69
hype-armor <gamm90@gmail.com>
2014-11-30 23:01:52 -0600

    Switching to Dragon for speech input.
    Removed .net speech rec.

--- House/Program.cs
+++ House/Program.cs
@@ -13,20 +13,17 @@ namespace House
 {
     class Program
     {
-        public static void Main(string[] args)
+        public void Main(string[] args)
         {
-            SearchEng s = new SearchEng();
-            string response = s.Search("how old is the oldest man");
-            say(response);
-
+            
             say("Hello, I am house.");
             start(null);
 
         }
 
-        private static void promps(string input)
+        private void promps(string input)
         {
-            
+            txtUnderstood.Text += input + Environment.NewLine;
             Weather w = new Weather();
 
             if (input.Contains("weather") || input.Contains("wheather"))
@@ -121,219 +118,12 @@ namespace House
 
         private static void say(string text)
         {
-            // Initialize a new instance of the SpeechSynthesizer.
-            SpeechSynthesizer synth = new SpeechSynthesizer();
-
-            // Configure the audio output. 
-            synth.SetOutputToDefaultAudioDevice();
-
-            // Speak a string.
-            synth.Speak(text);
-        }
-
-
-        private static SpeechRecognitionEngine LoadDictationGrammars()
-        {
-
-            // Create a default dictation grammar.
-            DictationGrammar defaultDictationGrammar = new DictationGrammar();
-            defaultDictationGrammar.Name = "default dictation";
-            defaultDictationGrammar.Enabled = true;
-
-            // Create the spelling dictation grammar.
-            DictationGrammar spellingDictationGrammar =
-              new DictationGrammar("grammar:dictation#spelling");
-            spellingDictationGrammar.Name = "spelling dictation";
-            spellingDictationGrammar.Enabled = true;
-
-            // Create the question dictation grammar.
-            DictationGrammar customDictationGrammar =
-              new DictationGrammar("grammar:dictation");
-            customDictationGrammar.Name = "question dictation";
-            customDictationGrammar.Enabled = true;
-
-            // Create a SpeechRecognitionEngine object and add the grammars to it.
-            SpeechRecognitionEngine recoEngine = new SpeechRecognitionEngine(new CultureInfo("en-US"));
-            recoEngine.LoadGrammar(defaultDictationGrammar);
-            recoEngine.LoadGrammar(spellingDictationGrammar);
-            recoEngine.LoadGrammar(customDictationGrammar);
-
-            // Add a context to customDictationGrammar.
-            customDictationGrammar.SetDictationContext("House, what is the", null);
-
-            return recoEngine;
-        }
-
-
-        // Indicate whether asynchronous recognition is complete.
-        static bool completed;
-
-        public static void start(string[] args)
-        {
-            // Create an in-process speech recognizer.
-            using (SpeechRecognitionEngine recognizer = new SpeechRecognitionEngine(new CultureInfo("en-US")))
+            if (!string.IsNullOrWhiteSpace(text))
             {
-                recognizer.UpdateRecognizerSetting("CFGConfidenceRejectionThreshold", 50);
-                recognizer.BabbleTimeout = new TimeSpan(0);
-                // Create a grammar for choosing cities for a flight.
-                Choices keyWords = new Choices(new string[] {"search for", "local news", "national news", "weather", "time", "temperature", "forecast", "panda" });
-
-                GrammarBuilder gb = new GrammarBuilder();
-                //gb.Append("House, what is the");
-                gb.Append(keyWords);
-                
-
-
-                // Construct a Grammar object and load it to the recognizer.
-                Grammar keywordChooser = new Grammar(gb);
-                keywordChooser.Name = ("City Chooser");
-                recognizer.LoadGrammarAsync(keywordChooser);
-
-                // Attach event handlers.
-                recognizer.SpeechDetected +=
-                  new EventHandler<SpeechDetectedEventArgs>(
-                    SpeechDetectedHandler);
-                recognizer.SpeechHypothesized +=
-                  new EventHandler<SpeechHypothesizedEventArgs>(
-                    SpeechHypothesizedHandler);
-                recognizer.SpeechRecognitionRejected +=
-                  new EventHandler<SpeechRecognitionRejectedEventArgs>(
-                    SpeechRecognitionRejectedHandler);
-                recognizer.SpeechRecognized +=
-                  new EventHandler<SpeechRecognizedEventArgs>(
-                    SpeechRecognizedHandler);
-                recognizer.RecognizeCompleted +=
-                  new EventHandler<RecognizeCompletedEventArgs>(
-                    RecognizeCompletedHandler);
-
-
-                // Assign input to the recognizer and start an asynchronous
-                // recognition operation.
-                recognizer.SetInputToDefaultAudioDevice();
-                while (true)
-                {
-                    completed = false;
-                    //Console.WriteLine("Starting asynchronous recognition...");
-                    
-                    recognizer.RecognizeAsync();
-
-                    // Wait for the operation to complete.
-                    while (!completed)
-                    {
-                        Thread.Sleep(30);
-                    }
-                    Console.WriteLine("Done.");
-                }
+                SpeechSynthesizer synth = new SpeechSynthesizer();
+                synth.SetOutputToDefaultAudioDevice();
+                synth.Speak(text);
             }
-        }
-
-        // Handle the SpeechDetected event.
-        static void SpeechDetectedHandler(object sender, SpeechDetectedEventArgs e)
-        {
-            //Console.WriteLine(" In SpeechDetectedHandler:");
-            Console.WriteLine(" - AudioPosition = {0}", e.AudioPosition);
-        }
-
-        // Handle the SpeechHypothesized event.
-        static void SpeechHypothesizedHandler(object sender, SpeechHypothesizedEventArgs e)
-        {
-            //Console.WriteLine(" In SpeechHypothesizedHandler:");
-
-            string grammarName = "<not available>";
-            string resultText = "<not available>";
-            if (e.Result != null)
-            {
-                if (e.Result.Grammar != null)
-                {
-                    grammarName = e.Result.Grammar.Name;
-                }
-                resultText = e.Result.Text;
-            }
-
-            //Console.WriteLine(" - Grammar Name = {0}; Result Text = {1}",
-            //  grammarName, resultText);
-        }
-
-        // Handle the SpeechRecognitionRejected event.
-        static void SpeechRecognitionRejectedHandler(object sender, SpeechRecognitionRejectedEventArgs e)
-        {
-            Console.WriteLine("RecognitionRejected: ");
-
-            string grammarName = "<not available>";
-            string resultText = "<not available>";
-            if (e.Result != null)
-            {
-                if (e.Result.Grammar != null)
-                {
-                    grammarName = e.Result.Grammar.Name;
-                    Console.WriteLine(e.Result.Confidence);
-                    say("say again");
-                }
-                resultText = e.Result.Text;
-            }
-
-            Console.WriteLine(" - Grammar Name = {0}; Result Text = {1}", grammarName, resultText);
-        }
-
-        // Handle the SpeechRecognized event.
-        static void SpeechRecognizedHandler(object sender, SpeechRecognizedEventArgs e)
-        {
-            //Console.WriteLine(" In SpeechRecognizedHandler.");
-
-            string grammarName = "<not available>";
-            string resultText = "<not available>";
-            if (e.Result != null)
-            {
-                if (e.Result.Grammar != null)
-                {
-                    grammarName = e.Result.Grammar.Name;
-                }
-                resultText = e.Result.Text;
-            }
-
-            Console.WriteLine(" - Grammar Name = {0}; Result Text = {1}",
-              grammarName, resultText);
-        }
-
-        // Handle the RecognizeCompleted event.
-        static void RecognizeCompletedHandler(object sender, RecognizeCompletedEventArgs e)
-        {
-            Console.WriteLine(" In RecognizeCompletedHandler.");
-
-            if (e.Error != null)
-            {
-                Console.WriteLine(
-                  " - Error occurred during recognition: {0}", e.Error);
-                return;
-            }
-            if (e.InitialSilenceTimeout || e.BabbleTimeout)
-            {
-                Console.WriteLine(
-                  " - BabbleTimeout = {0}; InitialSilenceTimeout = {1}",
-                  e.BabbleTimeout, e.InitialSilenceTimeout);
-                completed = true;
-                return;
-            }
-            if (e.InputStreamEnded)
-            {
-                Console.WriteLine(
-                  " - AudioPosition = {0}; InputStreamEnded = {1}",
-                  e.AudioPosition, e.InputStreamEnded);
-            }
-            if (e.Result != null)
-            {
-                Console.WriteLine(
-                  " - Grammar = {0}; Text = {1}; Confidence = {2}",
-                  e.Result.Grammar.Name, e.Result.Text, e.Result.Confidence);
-                promps(e.Result.Text);
-                
-                //Console.WriteLine(" - AudioPosition = {0}", e.AudioPosition);
-            }
-            else
-            {
-                Console.WriteLine(" - No result.");
-            }
-            completed = true;
         }
     }
 }
--- House/Weather.cs
+++ House/Weather.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
+using System.Speech.Synthesis;
 using System.Text;
 using System.Threading;
 using System.Windows;
@@ -22,36 +23,55 @@ class Weather
 
     static Weather()
 	{
-		string SavedLocation = "http://weather.yahooapis.com/forecastrss?z=" + ZIP;
-		// Create a new XmlDocument
-		XmlDocument Weather = new XmlDocument();
-		string rss = "http://xml.weather.yahoo.com/ns/rss/1.0";
-		Weather.Load(SavedLocation);
+        try
+        {
+            string SavedLocation = "http://weather.yahooapis.com/forecastrss?z=" + ZIP;
+            // Create a new XmlDocument
+            XmlDocument Weather = new XmlDocument();
+            string rss = "http://xml.weather.yahoo.com/ns/rss/1.0";
+            Weather.Load(SavedLocation);
 
-		// Set up namespace manager for XPath
-		XmlNamespaceManager NameSpaceMgr = new XmlNamespaceManager(Weather.NameTable);
+            // Set up namespace manager for XPath
+            XmlNamespaceManager NameSpaceMgr = new XmlNamespaceManager(Weather.NameTable);
 
-		NameSpaceMgr.AddNamespace("yweather", rss);
+            NameSpaceMgr.AddNamespace("yweather", rss);
 
-		// Get forecast with XPath
-		XmlNodeList condition = Weather.SelectNodes("/rss/channel/item/yweather:condition", NameSpaceMgr);
-		//XmlNodeList location = Weather.SelectNodes("/rss/channel/yweather:location", NameSpaceMgr);
-		XmlNodeList forecast = Weather.SelectNodes("/rss/channel/item/yweather:forecast", NameSpaceMgr);
+            // Get forecast with XPath
+            XmlNodeList condition = Weather.SelectNodes("/rss/channel/item/yweather:condition", NameSpaceMgr);
+            //XmlNodeList location = Weather.SelectNodes("/rss/channel/yweather:location", NameSpaceMgr);
+            XmlNodeList forecast = Weather.SelectNodes("/rss/channel/item/yweather:forecast", NameSpaceMgr);
 
-		_Condition = condition[0].Attributes["text"].Value;
-		_Temperature = condition[0].Attributes["temp"].Value + " degrees.";
-		string Fcast = "Today, " + forecast [0].Attributes ["text"].Value + " with a high a " +
-			forecast [0].Attributes ["high"].Value + " and a low of " +
-			forecast [0].Attributes ["low"].Value;
-		_Forecast = Fcast;
+            _Condition = condition[0].Attributes["text"].Value;
+            _Temperature = condition[0].Attributes["temp"].Value + " degrees.";
+            string Fcast = "Today, " + forecast[0].Attributes["text"].Value + " with a high a " +
+                forecast[0].Attributes["high"].Value + " and a low of " +
+                forecast[0].Attributes["low"].Value;
+            _Forecast = Fcast;
 
-        // Hazards
-        SavedLocation = "http://alerts.weather.gov/cap/wwaatmget.php?x=OKZ060&y=0";
-        Weather = new XmlDocument();
-        Weather.Load(SavedLocation);
-        NameSpaceMgr = new XmlNamespaceManager(Weather.NameTable);
+            // Hazards
+            SavedLocation = "http://alerts.weather.gov/cap/wwaatmget.php?x=OKZ060&y=0";
+            Weather = new XmlDocument();
+            Weather.Load(SavedLocation);
+            NameSpaceMgr = new XmlNamespaceManager(Weather.NameTable);
 
-        XmlElement hazards = Weather.DocumentElement["entry"]["summary"]; //Weather.SelectNodes("//feed/entry/summary", NameSpaceMgr);
-        _Hazards = hazards.InnerText;
+            XmlElement hazards = Weather.DocumentElement["entry"]["summary"]; //Weather.SelectNodes("//feed/entry/summary", NameSpaceMgr);
+            _Hazards = hazards.InnerText;
+        }
+        catch (Exception e)
+        {
+            say(e.Message);
+        }
 	}
+
+    private static void say(string text)
+    {
+        // Initialize a new instance of the SpeechSynthesizer.
+        SpeechSynthesizer synth = new SpeechSynthesizer();
+
+        // Configure the audio output. 
+        synth.SetOutputToDefaultAudioDevice();
+
+        // Speak a string.
+        synth.Speak(text);
+    }
 }
