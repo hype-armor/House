7392bd271b381661172fbaccf936794c8e34dfd9
Greg Morgan <gamm90@gmail.com>
2015-04-23 22:24:52 -0700

    WordsToNumbers dictionary was backwards. Fixed now.

--- House/WordsToNumbers.cs
+++ House/WordsToNumbers.cs
@@ -8,42 +8,42 @@ namespace OpenEcho
 {
     class WordsToNumbers
     {
-        public string retWord(int Num)
+        public int retInt(string word)
         {
 
-            Dictionary<int, string> WordList = new Dictionary<int,string>();
-            WordList.Add(0, "Zero");
-            WordList.Add(1, "One");
-            WordList.Add(2, "Two");
-            WordList.Add(3, "Three");
-            WordList.Add(4, "Four");
-            WordList.Add(5, "Five");
-            WordList.Add(6, "Six");
-            WordList.Add(7, "Seven");
-            WordList.Add(8, "Eight");
-            WordList.Add(9, "Nine");
+            Dictionary<string, int> WordList = new Dictionary<string, int>();
+            WordList.Add("Zero",       0);
+            WordList.Add("One",        1);
+            WordList.Add("Two",        2);
+            WordList.Add("Three",      3);
+            WordList.Add("Four",       4);
+            WordList.Add("Five",       5);
+            WordList.Add("Six",        6);
+            WordList.Add("Seven",      7);
+            WordList.Add("Eight",      8);
+            WordList.Add("Nine",       9);
                                         
-            WordList.Add(10, "Ten");
-            WordList.Add(11, "Eleven");
-            WordList.Add(12, "Twelve");
-            WordList.Add(13, "Thirteen");
-            WordList.Add(14, "Fourteen");
-            WordList.Add(15, "Fifteen");
-            WordList.Add(16, "Sixteen");
-            WordList.Add(17, "Seventeen");
-            WordList.Add(18, "Eighteen");
-            WordList.Add(19, "Nineteen");
+            WordList.Add("Ten",       10);
+            WordList.Add("Eleven",    11);
+            WordList.Add("Twelve",    12);
+            WordList.Add("Thirteen",  13);
+            WordList.Add("Fourteen",  14);
+            WordList.Add("Fifteen",   15);
+            WordList.Add("Sixteen",   16);
+            WordList.Add("Seventeen", 17);
+            WordList.Add("Eighteen",  18);
+            WordList.Add("Nineteen",  19);
                                         
-            WordList.Add(20, "Twenty");
-            WordList.Add(30, "Thirty");
-            WordList.Add(40, "Forty");
-            WordList.Add(50, "Fifty");
-            WordList.Add(60, "Sixty");
-            WordList.Add(70, "Seventy");
-            WordList.Add(80, "Eighty");
-            WordList.Add(90, "Ninety");
+            WordList.Add("Twenty",    20);
+            WordList.Add("Thirty",    30);
+            WordList.Add("Forty",     40);
+            WordList.Add("Fifty",     50);
+            WordList.Add("Sixty",     60);
+            WordList.Add("Seventy",   70);
+            WordList.Add("Eighty",    80);
+            WordList.Add("Ninety",    90);
 
-            return WordList[Num];
+            return WordList[word];
         }
     }
 }
