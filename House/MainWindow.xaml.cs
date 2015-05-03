e1adabc5a6a05e066263e33308e2f4b10980660f
Greg Morgan <gamm90@gmail.com>
2015-05-03 10:26:03 -0700

    Add QueryClassification to mainwindow.cs
    Removing the way I was classifying to incoming query.

--- House/MainWindow.xaml.cs
+++ House/MainWindow.xaml.cs
@@ -28,6 +28,7 @@ namespace OpenEcho
     {
         private static System.Timers.Timer aTimer;
         private Quartz quartz = new Quartz();
+        private QueryClassification qc = new QueryClassification();
 
         public MainWindow()
         {
@@ -36,17 +37,12 @@ namespace OpenEcho
             Speech.micMute.UnMuteMic();
             aTimer = new System.Timers.Timer(1000); 
             aTimer.Elapsed += Timeout;
-            //aTimer.Enabled = true;
             txtInput.Focus();
 
             Speech.micMute.UnMuteMic();
+
             quartz.Init();
-            
-
-
-            // google search test.
-            //SearchEng se = new SearchEng();
-            //se.Search("who is the president of the united states");
+            qc.Init();
         }
         private void Timeout(object sender, System.Timers.ElapsedEventArgs e)
         {
@@ -72,96 +68,26 @@ namespace OpenEcho
             input = input.Replace("  ", " ").Trim();
             int WordCount = input.Split(new char[] {' '}).Count();
 
-            string wikiSearchTerm = "search wikipedia for ";
-            if (string.IsNullOrWhiteSpace(input))
+            string wiki = "search wikipedia for";
+            string addAction = "add search term";
+            if (input.Contains(wiki))
             {
-                Speech.say("");
-            }
-            else if (input.StartsWith("what are ") || input.StartsWith("what is ") || input.StartsWith("what is a "))
-            {
-                input = input.Replace("what are ", "");
-                input = input.Replace("what is a ", "");
-                input = input.Replace("what is ", "");
-
-                // define term
-                Wikipedia wiki = new Wikipedia();
-                Speech.say(wiki.Search(input, true));
-            }
-            else if (input.StartsWith("how do you spell ") || input.StartsWith("spell ") || input.StartsWith("spell the word "))
-            {
-                // spell term
-
-                string word = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Last();
-                Speech.say(word + " is spelled.");
-                Thread.Sleep(1200);
-                foreach (char letter in word)
-                {
-                    Speech.say(letter.ToString());
-                    Thread.Sleep(700);
-                }
-            }
-            else if (input.StartsWith(wikiSearchTerm) || WordCount == 1)
-            {
-                input = input.Replace(wikiSearchTerm, "");
-
-                Wikipedia wiki = new Wikipedia();
-                Speech.say(wiki.Search(input));
-            }
-            else if (input == "time" || input == "what is the time" || input == "what time is it" || input == "what's the time")
-            {
-                Speech.say(DateTime.Now.ToShortTimeString());
-            }
-            else if (input.Contains("set alarm for"))
-            {
-                
-                string result = input.ReplaceWithNumbers();
-                string time = Regex.Match(result, @"\d+\s\d+|\d+").Value.Trim().Replace(" ", ":");
-                string ampm = Regex.Match(result, @"\s(?:am|pm)").Value.Trim();
-                quartz.CreateAlarm(DateTime.Parse(time + " " + ampm));
-                Speech.say(result.ToString());
-
-            }
-            else if (input.Contains("set timer"))
-            {
-                // input example: set timer for two hours and fourteen minutes.
-                // input will come as number hours and then number minutes. 
-
-                input = input.Replace("set timer for", "")
-                    .Replace("set timer", "")
-                    .Replace("and", ""); // best think of a better way to clean up useless words.
-
-                DateTime dt = DateTime.Now;
-                
-                if (input.Contains("hours"))
-                {
-                    string[] temp = input.Split(new string[] { "hours", "hour" }, StringSplitOptions.RemoveEmptyEntries);
-                    long hours = temp.First().Trim().ToLong();
-                    input = temp.Last().Trim();
-                   
-                    dt.AddHours(hours);
-                }
-                if (input.Contains("minutes"))
-                {
-                    string[] temp = input.Split(new string[] { "minutes" }, StringSplitOptions.RemoveEmptyEntries);
-                    long minutes = temp.First().Trim().ToLong();
-                    dt.AddMinutes(minutes);
-                }
-
-                Quartz q = new Quartz();
-                Speech.say(dt.ToShortTimeString());
-            }
-            else
-            {
-                // send the input to Wolfram to see if it can make sense of it.
-                Speech.say("Searching Wolfram Alpha");
-                Wolfram wa = new Wolfram();
-                string ret = wa.Query(input);
-                if (ret == "Wolfram|Alpha doesn't know how to interpret your input.")
-                {
-                    // send it to google?
-                    ret = "pardon?";
-                }
+                Wikipedia wikipedia = new Wikipedia();
+                string term = input.Replace(wiki, "");
+                string ret = wikipedia.Search(term);
                 Speech.say(ret);
+            }
+            else if (input.Contains(addAction))
+            {
+                // usage: add search term, x to y.
+                string tInput = input.Replace(addAction, "")
+                    .Trim();
+                string[] words = tInput.Split(new string[] { " to " }, StringSplitOptions.RemoveEmptyEntries);
+
+                string verb = words[0];
+                string action = words[1];
+
+                qc.AddVerbToAction(verb, action);
             }
         }
 
@@ -178,9 +104,8 @@ namespace OpenEcho
 
         private void cbSilent_Unchecked(object sender, RoutedEventArgs e)
         {
-            Speech.Silent = cbSilent.IsChecked == true ? true : false; // Because of type 'bool?'.
+            Speech.Silent = cbSilent.IsChecked == true ? true : false;
         }
 
-        
     }
 }
