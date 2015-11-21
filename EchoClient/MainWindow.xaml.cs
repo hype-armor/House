9b17700c98c305e5304f643a22b78b07faa96c1d
hype-armor <gamm90@gmail.com>
2015-11-20 21:58:32 -0600

    Create EchoClient

---
+++ EchoClient/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ExtensionMethods;
+
+namespace EchoClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void gobtn_Click(object sender, RoutedEventArgs e)
+        {
+            HitServer(txtQuery.Text.CleanText());
+        }
+
+        public void HitServer(string query = "")
+        {
+            WebClient client = new WebClient();
+
+            // Add a user agent header in case the 
+            // requested URI contains a query.
+
+            client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+
+            string serverPath = "http://192.168.0.50:8080/";
+
+            query = string.IsNullOrWhiteSpace(query) ? "" : query;
+
+            byte[] data = client.DownloadData(serverPath + query);
+
+            MediaPlayer mp = new MediaPlayer(data);
+            mp.Play();
+        }
+
+        public class MediaPlayer
+        {
+            System.Media.SoundPlayer soundPlayer;
+
+            public MediaPlayer(byte[] buffer)
+            {
+                var memoryStream = new MemoryStream(buffer, true);
+                soundPlayer = new System.Media.SoundPlayer(memoryStream);
+            }
+
+            public void Play()
+            {
+                soundPlayer.Play();
+            }
+
+            public void Play(byte[] buffer)
+            {
+                soundPlayer.Stream.Seek(0, SeekOrigin.Begin);
+                soundPlayer.Stream.Write(buffer, 0, buffer.Length);
+                soundPlayer.Play();
+            }
+        }
+    }
+}
