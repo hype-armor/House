1072416b823a4b12ae58bfa4741ddfa426d2cd6d
hype-armor <gamm90@gmail.com>
2015-11-26 17:34:18 -0600

    Fix audio not playing.

--- EchoClient/MainWindow.xaml.cs
+++ EchoClient/MainWindow.xaml.cs
@@ -34,10 +34,11 @@ namespace EchoClient
         {
             string text = txtQuery.Text.CleanText();
 
-            Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() =>
-            {
-                HitServer(text);
-            }));
+            //Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() =>
+            //{
+            //    HitServer(text);
+            //}));
+            HitServer(text);
         }
 
         public void HitServer(string query)
@@ -53,10 +54,10 @@ namespace EchoClient
 
             if (!string.IsNullOrWhiteSpace(query))
             {
-                byte[] data;
                 do
                 {
-                    data = client.DownloadData(serverPath + query);
+                    byte[] data = client.DownloadData(serverPath + query);
+                    MediaPlayer mp = new MediaPlayer(data);
                     byte[] guid = data.Take(16).ToArray();
                     data = data.Skip(16).ToArray();
 
@@ -67,13 +68,14 @@ namespace EchoClient
                         // server said the request will take a second. Check back with this GUID.
                         query = guid.GetString();
 
-                        MediaPlayer.Play(data);
+                        mp.Play();
                         Thread.Sleep(500);
                         continue;
                     }
                     else
                     {
-                        MediaPlayer.Play(data);
+                        mp.Play();
+                        return;
                     }
                 }
                 while (true);
@@ -82,14 +84,16 @@ namespace EchoClient
 
         public class MediaPlayer
         {
-            public static void Play(byte[] buffer)
+            System.Media.SoundPlayer soundPlayer;
+
+            public MediaPlayer(byte[] buffer)
             {
-                System.Media.SoundPlayer soundPlayer;
                 var memoryStream = new MemoryStream(buffer, true);
                 soundPlayer = new System.Media.SoundPlayer(memoryStream);
+            }
 
-                soundPlayer.Stream.Seek(0, SeekOrigin.Begin);
-                soundPlayer.Stream.Write(buffer, 0, buffer.Length);
+            public void Play()
+            {
                 soundPlayer.Play();
             }
         }
