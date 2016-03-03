096aaaef1b9fb38dbd8c67f64606b846917491c5
hype-armor <gamm90@gmail.com>
2016-03-02 18:38:59 -0600

    Removed SQL code.

--- EchoServer/MessageSystem.cs
+++ EchoServer/MessageSystem.cs
@@ -16,9 +16,7 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-using Extensions;
 using System;
-using System.Data.SqlClient;
 
 namespace EchoServer
 {
@@ -42,96 +40,5 @@ namespace EchoServer
         public DateTime postTime = new DateTime();
         public enum Status { queued, processing, delayed, ready, closed, error };
         public Status status = Status.queued;
-    }
-
-    public class SQL_old
-    {
-        private static string connectionString = "Server=SKYNET\\SQLEXPRESS;Database=Echo;Trusted_Connection=True;";
-
-        public static Message GetNextMessage()
-        {
-            using (SqlConnection con = new SqlConnection(connectionString))
-            {
-                Guid msgGuid = GetNextMessageGuid();
-
-                con.Open();
-                using (SqlCommand command = new SqlCommand("SELECT [MessageID],[ClientID],[textRequest], " +
-                    "[textResponse],[request],[response],[PostTime],[Status]" +
-                    " FROM Messages " + "WHERE [MessageID]=@MessageID", con))
-                {
-                    command.Parameters.Add(new SqlParameter("MessageID", msgGuid));
-                    SqlDataReader reader = command.ExecuteReader();
-
-                    Message m = new Message();
-                    while (reader.Read())
-                    {
-                        m.messageID = Guid.Parse(reader["MessageID"].ToString());
-                        m.clientID = Guid.Parse(reader["ClientID"].ToString());
-                        m.textRequest = reader["textRequest"].ToString();
-                        m.textResponse = reader["textResponse"].ToString();
-                        m.audioRequest = reader["request"].ToByteArray();
-                        m.audioResponse = reader["response"].ToByteArray();
-                        m.postTime = reader["PostTime"].ToDateTime();
-                        m.status = (Message.Status)reader["Status"].ToInt();
-
-                    }
-                    return m;
-                }
-            }
-        }
-
-        public static void UpdateMessage(Message msg)
-        {
-            using (SqlConnection con = new SqlConnection(connectionString))
-            {
-                con.Open();
-
-                using (SqlCommand command = new SqlCommand(
-                    "UPDATE [Messages] " +
-                    "SET [textRequest]=@textRequest,[textResponse]=@textResponse,[request]=@request, " +
-                    "[response]=@response,[Status]=@Status,[LastUpdated]=GETDATE() " +
-                    "WHERE [MessageID]=@MessageID", con))
-                {
-                    command.Parameters.Add(new SqlParameter("textRequest", msg.textRequest));
-                    command.Parameters.Add(new SqlParameter("textResponse", msg.textResponse));
-                    command.Parameters.Add(new SqlParameter("request", msg.audioRequest));
-                    command.Parameters.Add(new SqlParameter("response", msg.audioResponse));
-                    command.Parameters.Add(new SqlParameter("Status", msg.status));
-                    command.Parameters.Add(new SqlParameter("MessageID", msg.messageID));
-                    command.ExecuteNonQuery();
-                }
-            }
-        }
-
-        public static Guid GetNextMessageGuid()
-        {
-            Guid msgGuid = Guid.Empty;
-            using (SqlConnection con = new SqlConnection(connectionString))
-            {
-                con.Open();
-                using (SqlCommand command = new SqlCommand("DECLARE @msg uniqueidentifier; " +
-                    "SELECT TOP 1 @msg = [MessageID] " +
-                    "FROM Messages " +
-                    "WHERE [Status] = 0 " +
-                    "ORDER BY [PostTime] ASC; " +
-                    "SELECT @msg AS [MessageID]; " +
-                    "UPDATE [Messages] " +
-                    "SET [Status] = 1, [LastUpdated]=GETDATE()" +
-                    "WHERE [MessageID] = @msg; ", con))
-                {
-                    SqlDataReader reader = command.ExecuteReader();
-                    while (reader.Read())
-                    {
-                        if (!string.IsNullOrWhiteSpace(reader["MessageID"].ToString()))
-                        {
-                            msgGuid = Guid.Parse(reader["MessageID"].ToString());
-                        }
-                    }
-                }
-                con.Close();
-            }
-
-            return msgGuid;
-        }
     }
 }
